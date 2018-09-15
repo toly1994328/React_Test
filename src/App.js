3d1cf1accd7cb9edb8120f9ed2bb872d52b951fc
toly <1981462002@qq.com>
2018-09-15 11:22:12 +0800

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+          <div >
+              hello-toly
+          </div>
+      </div>
+    );
+  }
+}
+
+export default App;
